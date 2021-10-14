8a25562af82e7e5fca69d5381ca0c72cd3aad8ca
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-10-14 23:05:40 +0900

    Detail Container 1

---
+++ yeflix/src/Routes/Detail/DetailContainer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import DetailPresenter from "./DetailPresenter";
+
+export default class extends React.Component{
+  state={
+    result:null,
+    error:null,
+    loading:true
+  }
+  async componentDidMount(){
+    const {
+      match:{params:{id}},
+      location:{push}
+    }=this.props;
+    const parsedId=parseInt(id);
+    if(parsedId===NaN){
+      push("/");
+    }
+  }
+  render(){
+    const {result,error,loading}=this.state;
+    return (
+      <DetailPresenter 
+       result={result} 
+       error={error}
+       loading={loading}
+      />
+    );
+  }
+
+}
