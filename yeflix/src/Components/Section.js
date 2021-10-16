0b4c89600569bd89616d8691157600f578bcde1d
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-10-16 11:19:50 +0900

    HomePresenter

--- yeflix/src/Components/Section.js
+++ yeflix/src/Components/Section.js
@@ -1,11 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
-const Section=({title,children})=>{};
+const Container=styled.div`
+  margin-bottom:50px;
+`;
+const Title=styled.span`
+   font-size:14px;
+   font-weight:600;
+   
+`;
+const Grid=styled.div`
+margin-top:25px;
+`;
+
+const Section=({title,children})=>{
+   <Container>
+     <Title>{title}</Title>
+     <Grid>{children}</Grid>
+   </Container>
+
+};
 
 Section.propTypes={
   title:PropTypes.string.isRequired,
-  children:PropTypes.oneOfType({})
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
 }
-
+//node : 렌더링할 수 있는 모든 것 (숫자, 문자열, 혹은 JSX코드. children도 node PropType임)
 export default Section;
