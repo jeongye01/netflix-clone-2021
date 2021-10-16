c56055f88677a56bc2b132b74162be458d66e57b
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-10-16 11:39:26 +0900

    TVPresenter and Loader Components

--- yeflix/src/Components/Section.js
+++ yeflix/src/Components/Section.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const Container=styled.div`
-  margin-bottom:50px;
+  :not(:last-child){margin-bottom:50px;}
 `;
 const Title=styled.span`
    font-size:14px;
@@ -11,7 +11,11 @@ const Title=styled.span`
    
 `;
 const Grid=styled.div`
-margin-top:25px;
+  margin-top:25px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 125px);
+  grid-gap: 25px;
+
 `;
 
 const Section=({title,children})=>{
