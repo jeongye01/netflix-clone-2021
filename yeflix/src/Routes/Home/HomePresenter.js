14c0c9e07ca9049501f892a5d9ad37ff1d4de9cc
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-10-19 01:03:58 +0900

    poster

--- yeflix/src/Routes/Home/HomePresenter.js
+++ yeflix/src/Routes/Home/HomePresenter.js
@@ -4,7 +4,8 @@ import styled from "styled-components";
 
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
-
+import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 const Container=styled.div`
 padding: 0px 20px;
 `;
@@ -13,25 +14,49 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =><Loa
      {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+         <Poster
+         key={movie.id}
+         id={movie.id}
+         imageUrl={movie.poster_path}
+         title={movie.original_title}
+         rating={movie.vote_average}
+         year={movie.release_date.substring(0, 4)}
+         isMovie={true}
+       />
           ))}
         </Section>
       )}
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Movies">
           {upcoming.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster
+            key={movie.id}
+            id={movie.id}
+            imageUrl={movie.poster_path}
+            title={movie.original_title}
+            rating={movie.vote_average}
+            year={movie.release_date.substring(0, 4)}
+            isMovie={true}
+          />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Movies">
           {popular.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+          <Poster
+          key={movie.id}
+          id={movie.id}
+          imageUrl={movie.poster_path}
+          title={movie.original_title}
+          rating={movie.vote_average}
+          year={movie.release_date.substring(0, 4)}
+          isMovie={true}
+        />
           ))}
         </Section>
       )}
-
+{error && <Message color="#e74c3c" text={error} />}
   </Container>);
 
  
--- yeflix/src/Routes/TV/TVPresenter.js
+++ yeflix/src/Routes/TV/TVPresenter.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Message from "../../Components/Message";
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 0px 20px;
 `;
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>loading ? (
   <Loader />
@@ -10,19 +11,47 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>loadi
   (<Container>
     {topRated && topRated.length > 0 && (
       <Section title="Top Rated Shows">
-        {topRated.map(show => show.name)}
+         {topRated.map(show => (
+           <Poster
+           key={show.id}
+           id={show.id}
+           imageUrl={show.poster_path}
+           title={show.original_name}
+           rating={show.vote_average}
+           year={show.first_air_date.substring(0, 4)}
+         />
+          ))}
       </Section>
     )}
     {popular && popular.length > 0 && (
       <Section title="Popular Shows">
-        {popular.map(show => show.name)}
+        {popular.map(show => (
+           <Poster
+           key={show.id}
+           id={show.id}
+           imageUrl={show.poster_path}
+           title={show.original_name}
+           rating={show.vote_average}
+           year={show.first_air_date.substring(0, 4)}
+         />
+          ))}
       </Section>
     )}
     {airingToday && airingToday.length > 0 && (
       <Section title="Airing Today">
-        {airingToday.map(show => show.name)}
+        {airingToday.map(show => (
+           <Poster
+           key={show.id}
+           id={show.id}
+           imageUrl={show.poster_path}
+           title={show.original_name}
+           rating={show.vote_average}
+           year={show.first_air_date.substring(0, 4)}
+         />
+          ))}
       </Section>
     )}
+     {error && <Message color="#e74c3c" text={error} />}
   </Container>);
 
 TVPresenter.propTypes = {
